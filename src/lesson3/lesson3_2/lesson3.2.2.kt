c3af601ce16a21588750e6fe9e3d6e13806805ab
sangholim01 <sangholim01@hobbyful.co.kr>
2021-11-06 15:58:44 +0900

    - 소스코드 누락 반영

---
+++ src/lesson3/lesson3_2/lesson3.2.2.kt
@@ -0,0 +1,22 @@
+package lesson3.lesson3_2
+
+import java.lang.StringBuilder
+
+fun <T> joinToString2(
+    collection: Collection<T>,
+    separator: String = ", ",
+    prefix: String = "",
+    postFix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postFix)
+    return result.toString()
+}
+
+fun main() {
+    println(joinToString2(collection = listOf(1, 2, 3), separator = ";", prefix = "(", postFix = ")"));
+}
